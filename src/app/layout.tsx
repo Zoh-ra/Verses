7f132a75210e65ee875f4ac86ff14e509e0d5c5a
zoh-ra <raitonoums@gmail.com>
2025-03-22 20:17:12 +0100

    Déplacer les balises PWA vers _document.tsx pour un meilleur support d'installation sur mobile

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -39,21 +39,7 @@ export default function RootLayout({
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
-        <meta name="application-name" content="Verses - Quran App" />
-        <meta name="apple-mobile-web-app-capable" content="yes" />
-        <meta name="apple-mobile-web-app-status-bar-style" content="default" />
-        <meta name="apple-mobile-web-app-title" content="Verses" />
-        <meta name="format-detection" content="telephone=no" />
-        <meta name="mobile-web-app-capable" content="yes" />
-        <meta name="msapplication-TileColor" content="#781ECF" />
-        <meta name="msapplication-tap-highlight" content="no" />
-        
-        {/* Balises PWA spécifiques supplémentaires */}
-        <link rel="manifest" href="/manifest.json" />
-        <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
-        <meta name="theme-color" content="#781ECF" />
-        
-        <script src="/register-sw.js" defer></script>
+        {/* Toutes les balises meta spécifiques ont été déplacées vers _document.tsx */}
       </head>
       <body className={`${geistSans.variable} ${geistMono.variable}`}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
---
+++ src/pages/_document.tsx
@@ -0,0 +1,35 @@
+import Document, { Html, Head, Main, NextScript } from 'next/document';
+
+class MyDocument extends Document {
+  render() {
+    return (
+      <Html lang="fr">
+        <Head>
+          {/* Balises PWA essentielles */}
+          <link rel="manifest" href="/manifest.json" />
+          <link rel="apple-touch-icon" href="/icons/icon-192x192.png" />
+          <meta name="theme-color" content="#781ECF" />
+          
+          {/* Autres méta-balises PWA */}
+          <meta name="application-name" content="Verses - Quran App" />
+          <meta name="apple-mobile-web-app-capable" content="yes" />
+          <meta name="apple-mobile-web-app-status-bar-style" content="default" />
+          <meta name="apple-mobile-web-app-title" content="Verses" />
+          <meta name="format-detection" content="telephone=no" />
+          <meta name="mobile-web-app-capable" content="yes" />
+          <meta name="msapplication-TileColor" content="#781ECF" />
+          <meta name="msapplication-tap-highlight" content="no" />
+          
+          {/* Script d'enregistrement du service worker */}
+          <script src="/register-sw.js" defer></script>
+        </Head>
+        <body>
+          <Main />
+          <NextScript />
+        </body>
+      </Html>
+    );
+  }
+}
+
+export default MyDocument;
